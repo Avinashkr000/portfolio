f2593bed9663cec77d28ed9a6a9be5a01f45c99e
Avinash kumar <akum97543@gmail.com>
2025-08-02 17:21:51 +0530

    Add Resume download section (resume controller + PDF link)

---
+++ src/main/java/com/avinash/portfoliio/portfolio/controller/ResumeController.java
@@ -0,0 +1,12 @@
+package com.avinash.portfoliio.portfolio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ResumeController {
+    @GetMapping("/resume")
+    public String resume() {
+        return "resume";
+    }
+}
